5554c571e786b1d5097bca334208795499ca3818
bharaths1803 <bharaths14051803@gmail.com>
2025-08-15 19:23:16 +0530

    Changed query params

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -13,7 +13,7 @@ const WatchPage = () => {
           <iframe
             width="1000"
             height="600"
-            src="https://www.youtube.com/embed/MZETIoZlLC4?si=bYYSeZLn8yyfN3-k"
+            src={"https://www.youtube.com/embed/" + searchParams.get("v")}
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
